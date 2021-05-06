e706eaaae42523c71b16ccda47a26f97c7812f6c
Jason <jason@google.com>
2021-05-06 05:41:52 -0400

    add exclude

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 const reader = require('xlsx')
 const http = require('http');
 const stores = new Map();
+const csv = require('csv-parser');
+const fs = require('fs')
+const excludes = [];
+const appStart = new Date();
+
+const loadExclude = () => {
+  return new Promise((resolve, reject)=>{
+    fs.createReadStream('./files/exclude.csv')
+      .pipe(csv())
+      .on('data', (data) => excludes.push(data))
+      .on('end', () => {
+        resolve({
+          exc: excludes
+        });
+        // [
+        //   {
+        //     service: 'position/positionDecisionAnalysis?collectiveId=720&activityDateId=10759&isJmr=1&reportPeriod=1y&benchmark=FTSE%20All%20World&field=exposure'
+        //   },
+        //   {
+        //     service: 'securities/securityFactorModelFactorList?permissionId=1&modelId=30&securityId=40905&sedol=BH4HKS3'
+        //   }
+        // ]
+      })
+      .on('error', ()=>{
+        reject({
+          exc: []
+        })
+      });
+  });
+}
 
 const sendRequest = (url, msg) => {
   return new Promise((resolve, reject) => {
@@ -35,9 +65,9 @@ const api = 'http://172.20.17.108:60016/v1/data/{service}&clientId=1&permissionI
 const sheets = file.SheetNames;
 
 const pools = [];
-let count = 0;
-for (let i = 0; i < sheets.length; i++) {
-  const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]])
+// let count = 0;
+// for (let i = 0; i < sheets.length; i++) {
+  const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[0]])
   temp.forEach(({ Services }) => {
     // if (count > 3) return;
     // count++;
@@ -51,20 +81,27 @@ for (let i = 0; i < sheets.length; i++) {
     }
     stores.set(Services, null);
   })
-}
+// }
 
 const iter = stores.keys();
 const size = stores.size;
 let cursor = iter.next();
-let count = 0;
+let count = 1;
 
 async function load() {
+  const { exc } = await loadExclude();
   while (!!cursor.value) {
     try {
       const url = api.replace('{service}', cursor.value);
       let msg = `${count}th of ${size}`;
-      const { statusCode, spend } = await sendRequest(url, msg);
-      stores.set(cursor.value, { status: statusCode, spend: spend });
+      let findExc = exc && exc.length > 0 && exc.find(excs=>excs.service === cursor.value);
+      if(!!findExc){
+        console.log('****Skip for Slow Service****');
+        stores.set(cursor.value, { status: 301, spend: 0 });
+      }else{
+        const { statusCode, spend } = await sendRequest(url, msg);
+        stores.set(cursor.value, { status: statusCode, spend: spend });
+      }
     } catch (error) {
       console.error('Fail on request');
       stores.set(cursor.value, { status: 500, spend: 0 });
@@ -95,7 +132,9 @@ async function load() {
 }
 
 
-load().catch(e=>console.error(e));
+load().catch(e=>console.error(e)).finally(()=>{
+  console.log(`${Math.round((new Date() - appStart) / 1000 * 10) / 10 / 60} min `);
+});
 
 
 
